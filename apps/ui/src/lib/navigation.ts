669b7c93deca41e980f827de4894ce41b1c2aacc
Sergej Stroh <sergej.stroh@themeart.de>
2026-06-28 14:36:02 +0200

    Update navigation.ts
    de, en

--- apps/ui/src/lib/navigation.ts
+++ apps/ui/src/lib/navigation.ts
@@ -7,10 +7,10 @@ import { getEnvVar } from "@/lib/env-vars"
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["cs", "en"],
+  locales: ["de", "en"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "de",
 
   localePrefix: "as-needed",
 })
