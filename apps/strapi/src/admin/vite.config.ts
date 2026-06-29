4c5aa65d887d63f7c8683c1ee133c49edfc4a3a4
Sergej Stroh <sergej.stroh@themeart.de>
2026-06-29 20:20:55 +0200

    Update vite.config.ts

--- apps/strapi/src/admin/vite.config.ts
+++ apps/strapi/src/admin/vite.config.ts
@@ -3,12 +3,14 @@
 import { mergeConfig, type UserConfig } from "vite"
 
 export default (config: UserConfig) => {
-  // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
       alias: {
         "@": "/src",
       },
+    },
+    server: {
+      allowedHosts: ["cms.tugus.de"],
     },
   } as UserConfig)
 }
